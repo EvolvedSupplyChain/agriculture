3bd9b5d4d5d3a280d32e4c0133467a1dbad26002
EvolvedSupplyChain <161888284+EvolvedSupplyChain@users.noreply.github.com>
2024-03-01 16:44:52 -0700

    Update main.py
    Minor change to force OTA update for testing

--- main.py
+++ main.py
@@ -365,7 +365,7 @@ def main():
         client.publish(secretVars.telemTopic, json.dumps(testMsg).encode())
         
         #collect garbage and close files
-        
+        print("meaningless change for update test")
         time.sleep(5)
 
 main()
