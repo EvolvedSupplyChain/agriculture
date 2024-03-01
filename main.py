8eea5fe37ba0d7867ca54a4c410735c5afd30c06
EvolvedSupplyChain <161888284+EvolvedSupplyChain@users.noreply.github.com>
2024-03-01 16:50:05 -0700

    Update main.py
    OTA update testing

--- main.py
+++ main.py
@@ -365,7 +365,7 @@ def main():
         client.publish(secretVars.telemTopic, json.dumps(testMsg).encode())
         
         #collect garbage and close files
-        print("meaningless change for update test")
+        print("meaningless change for update test, take two")
         time.sleep(5)
 
 main()
