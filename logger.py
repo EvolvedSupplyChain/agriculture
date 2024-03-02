e3f836faae00cbce4cc0080680c0c5059b50ca7f
EvolvedSupplyChain <161888284+EvolvedSupplyChain@users.noreply.github.com>
2024-03-02 13:21:45 -0700

    Update logger.py

--- logger.py
+++ logger.py
@@ -372,7 +372,7 @@ def main():
                    "lux": fullLux,
                    "spectral": specData,
                    "imageData": "pqmq1fqhVG7gaNMpWMQx8A==",
-                   "softwareVersion": 1.4
+                   "softwareVersion": 1.5
                    }
                  
         print(json.dumps(testMsg).encode())
